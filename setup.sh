d2c9315b32b3969c9dacf352966626c60f5eb8d9
Dano Sherin Jose <danorul9@gmail.com>
2020-05-10 15:23:15 +1000

    Adding Public Share

--- setup.sh
+++ setup.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
+#updating the apt packages index
+sudo apt update
 
-echo "Hello World"
+#Install the Samba package
+sudo apt install samba
+
+#check whether the Samba server is running
+sudo systemctl status smbd
+
+#allow incoming UDP connections on ports 137 and 138 and TCP connections on ports 139 and 445
+sudo ufw allow 'Samba'
+
+
+#backup the original samba configuration file 
+sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.orig
+
+#creating a shared samba directory where the files will be stored
+sudo mkdir -p /srv/samba/anonymous_shares
+
+#set the appropriate permissions on the directory.
+sudo chmod -R 0775 /srv/samba/anonymous_shares
+sudo chown -R nobody:nogroup /srv/samba/anonymous_shares
+
+#Update config from git
+sudo cp ./etc/samba/smb.conf /etc/samba/smb.conf
+
+#restart the Samba services
+sudo systemctl restart smbd
+sudo systemctl restart nmbd
+
